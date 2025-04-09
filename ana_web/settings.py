14856332c906730f1e5c49b869cb73e89795dcc0
sudhansut5 <pandit0776@gmail.com>
2025-04-09 19:32:03 +0530

    Set DEBUG to False and updated ALLOWED_HOSTS

--- ana_web/settings.py
+++ ana_web/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oy1snd+pefg_u)nqg2wme3(lgblm&4bl*t!kt97+r2f6pbijv4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cwmstest-github.onrender.com']
+
 
 
 # Application definition
